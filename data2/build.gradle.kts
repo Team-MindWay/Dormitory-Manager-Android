5f419d54a068b37c1da149cc768091cdfd158c60
kimjeakwan <s24042@gsm.hs.kr>
2024-09-29 19:30:41 +0900

    :heavy_plus_sign: :: 모듈간의 의존성 추가

--- data2/build.gradle.kts
+++ data2/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":domain"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
