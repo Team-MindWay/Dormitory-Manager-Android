4a85981f864439bd9d7461c97fd451fb195341d3
kimjeakwan <s24042@gsm.hs.kr>
2024-10-03 17:32:55 +0900

    :lipstick: :: Rankingcomponent Ui 구현

--- presentation/src/main/java/view/main/component/Rankingcomponent.kt
+++ presentation/src/main/java/view/main/component/Rankingcomponent.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -14,17 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun Timecomponent(
+fun Rankingcomponent(
     modifier: Modifier=Modifier
 ){
     Column (
@@ -74,6 +70,6 @@ fun Timecomponent(
 
 @Composable
 @Preview
-fun PreviewTimeComponent(){
-    Timecomponent()
+fun PreviewRankingComponent(){
+   Rankingcomponent()
 }
