df4a0129ade1056387be1e3d0b6d06a21a0f6305
kimjeakwan <s24042@gsm.hs.kr>
2024-10-04 17:26:35 +0900

    :memo: ::  쓰지 않는 import문 삭제

--- presentation/src/main/java/view/login/component/DomaGathbutton.kt
+++ presentation/src/main/java/view/login/component/DomaGathbutton.kt
@@ -1,23 +1,16 @@
 package view.login.component
 
-import android.service.autofill.OnClickAction
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,7 +54,5 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 fun DomaGauthbuttonPrevies(){
-    DomaGathbutton {
-
-    }
+    DomaGathbutton {}
 }
--- presentation/src/main/java/view/main/Screen/mainscreen.kt
+++ presentation/src/main/java/view/main/Screen/mainscreen.kt
@@ -9,29 +9,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kim.presentation.R
 import view.main.component.MyRankingcomponent
 import view.main.component.MypageButton
 import view.main.component.Rankingcomponent
@@ -65,7 +56,7 @@ fun mainscreen (
 
         ){
 
-                MypageButton()
+                MypageButton(){}
 
 
         }
@@ -210,6 +201,7 @@ fun mainscreen (
 fun PreviewMainScreen(){
 
     mainscreen(
+
 
         rankingcomponentltemData = listOf(
             RankingcomponentltemData(Rank = "5등", name = "김재관", number = "5회"),
