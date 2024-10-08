fbeaf113b51efe95e66d8821de746107fdfea2c0
kimjeakwan <s24042@gsm.hs.kr>
2024-10-09 04:13:40 +0900

    ✨: LoginNavigation

---
+++ presentation/src/main/java/view/login/navigation/LoginNavigation.kt
@@ -0,0 +1,25 @@
+package view.login.navigation
+
+
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import view.login.screen.LoginScreen
+
+
+const val loginRoute = "login_route"
+
+fun NavController.navigationToLogin() {
+    this.navigate(loginRoute)
+}
+
+fun NavGraphBuilder.loginScreen(
+    navigateToHome: () -> Unit
+) {
+    composable(loginRoute) {
+        LoginScreen(
+            navigateToHome = navigateToHome
+        )
+    }
+}
