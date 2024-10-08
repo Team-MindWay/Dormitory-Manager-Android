40ca632803d20016ed05e601b0671c09319e21f1
kimjeakwan <s24042@gsm.hs.kr>
2024-10-09 04:14:57 +0900

    ♻️ MainActivity 에 DMNavhost 추가

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -3,7 +3,9 @@ package com.kim.Dormitorymanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import view.login.navigation.loginRoute
 import view.login.screen.LoginScreen
 
 
@@ -12,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            LoginScreen()
+            DMNavHost(navController = rememberNavController(), startDestination = loginRoute)
         }
     }
 
