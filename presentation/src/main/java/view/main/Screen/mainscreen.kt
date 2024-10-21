8291adc8a56b32d3bd3761f45666f392b8f4864c
kimjeakwan <s24042@gsm.hs.kr>
2024-10-21 16:02:20 +0900

    ♻️: 의존성 변경

--- presentation/src/main/java/view/main/Screen/mainscreen.kt
+++ presentation/src/main/java/view/main/Screen/mainscreen.kt
@@ -1,8 +1,7 @@
 package view.main.Screen
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
