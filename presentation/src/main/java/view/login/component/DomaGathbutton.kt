067ac882ec65c9592a454e11fb1f6905e9a078bb
kimjeakwan <s24042@gsm.hs.kr>
2024-10-23 20:58:17 +0900

    ♻️: stable 어노테이션 추가

--- presentation/src/main/java/view/login/component/DomaGathbutton.kt
+++ presentation/src/main/java/view/login/component/DomaGathbutton.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,7 +17,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kim.presentation.component.modifier.clickablesingle
-
+@Stable
 @Composable
  fun DomaGathbutton (
      modifier: Modifier=Modifier,
