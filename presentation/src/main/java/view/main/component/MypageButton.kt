c8813a45d2522fb45092ac8a5ffc85392be79427
kimjeakwan <s24042@gsm.hs.kr>
2024-10-23 20:58:22 +0900

    ♻️: stable 어노테이션 추가

--- presentation/src/main/java/view/main/component/MypageButton.kt
+++ presentation/src/main/java/view/main/component/MypageButton.kt
@@ -9,21 +9,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kim.presentation.R
 
+@Stable
 @Composable
 fun MypageButton(
     modifier: Modifier=Modifier,
