a2b48793d28c12297c87214c67f8af7d852613e2
kimjeakwan <s24042@gsm.hs.kr>
2025-01-23 12:53:26 +0900

    :sparkles: :: UsersResponse

---
+++ data/src/main/java/remote/dto/users/response/UsersResponse.kt
@@ -0,0 +1,33 @@
+package remote.dto.users.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import model.myrank.response.MyRankResponseModel
+import model.users.response.UsersResponseModel
+import remote.dto.myrank.response.MyRankResponse
+import java.util.UUID
+
+@JsonClass(generateAdapter = true)
+data class UsersResponse (
+    @Json(name = "userId") val userId: UUID,
+    @Json(name = "clean-Point") val cleanPoint: Int,
+    @Json(name = "to-day-Clean") val todayClean: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "penalty-Point") val penaltyPoint: Int,
+    @Json(name = "Mybecause") val Mybecause: String,
+    @Json(name = "because") val because: String,
+    @Json(name = "Point-List") val PointList: Int,
+
+)
+
+fun UsersResponse.toModel() = UsersResponseModel(
+    userId = userId,
+    cleanPoint = cleanPoint,
+    todayClean = todayClean,
+    name = name,
+    penaltyPoint = penaltyPoint,
+    Mybecause = Mybecause,
+    because = because,
+    PointList = PointList
+
+)
