48791917b2df74fe8b118b11949672d236771a4d
kimjeakwan <s24042@gsm.hs.kr>
2025-01-24 10:33:44 +0900

    :recycle: :: homes 과련 import 문 삭제

--- presentation/src/main/java/viewModel/users/UsersViewModel.kt
+++ presentation/src/main/java/viewModel/users/UsersViewModel.kt
@@ -10,27 +10,22 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import usecase.Users.GetUsersUseCase
-import usecase.homes.GetRankUseCase
 import viewModel.UIstate.UsersUiState
-
 import javax.inject.Inject
-
-
 
 @HiltViewModel
 class UsersViewModel @Inject constructor(
     private val getUsersUseCase: GetUsersUseCase,
-    ) : ViewModel() {
+) : ViewModel() {
     private val _usersUiState = MutableStateFlow<UsersUiState>(UsersUiState.Loading)
     internal val usersUiState = _usersUiState.asStateFlow()
-
 
     fun getUsers() = viewModelScope.launch {
         getUsersUseCase()
             .asResult()
             .collectLatest { result ->
                 when (result) {
-                    is Untill.Result.Loading -> {
+                    is Result.Loading -> {
                         _usersUiState.value = UsersUiState.Loading
                     }
 
@@ -43,34 +38,10 @@ class UsersViewModel @Inject constructor(
                             _usersUiState.value = UsersUiState.Empty
                         } else {
                             _usersUiState.value = UsersUiState.Success(result.data)
-
-
                         }
                     }
                 }
             }
-
-        fun getMyRank() = viewModelScope.launch {
-            getUsersUseCase()
-                .asResult()
-                .collectLatest { result ->
-                    when (result) {
-                        is Result.Loading -> _usersUiState.value = UsersUiState.Loading
-                        is Result.Error -> _usersUiState.value = UsersUiState.Fail
-                        is Result.Success -> {
-                            if (result.data.isEmpty()) {
-                                _usersUiState.value = UsersUiState.Empty
-                            } else {
-                                _usersUiState.value = UsersUiState.Success(result.data)
-                            }
-
-                        }
-
-                    }
-                }
-        }
-
-
     }
 }
 
