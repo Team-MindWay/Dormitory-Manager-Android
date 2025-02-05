8d1656b8e6659f48b77ed61cc4b222ec2d3ffcee
kimjeakwan <s24042@gsm.hs.kr>
2025-02-05 16:02:28 +0900

    :recycle: :: packgename 이름을 대문자에서 전부 소문자로 변경

--- presentation/src/main/java/viewModel/users/uistate/UsersUiState.kt
+++ presentation/src/main/java/viewModel/users/uistate/UsersUiState.kt
@@ -1,10 +1,11 @@
-package viewModel.UIstate
+package viewmodel.users.uistate
 
+import kotlinx.collections.immutable.ImmutableList
 import model.users.response.UsersResponseModel
 
 sealed interface UsersUiState {
     object Loading : UsersUiState
     object Empty : UsersUiState
-    data class Success(val data: List<UsersResponseModel>) : UsersUiState
+    data class Success(val data: ImmutableList<UsersResponseModel>) : UsersUiState
     object Fail : UsersUiState
 }
