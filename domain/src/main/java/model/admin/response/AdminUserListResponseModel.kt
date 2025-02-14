5e0011751934c60e3e2dc889ecdadcfd227a1c2a
kimjeakwan <s24042@gsm.hs.kr>
2025-02-14 17:22:33 +0900

    :sparkles: :: AdminUserListResponse.kt

---
+++ domain/src/main/java/model/admin/response/AdminUserListResponseModel.kt
@@ -0,0 +1,10 @@
+package model.admin.response
+
+import java.util.UUID
+
+data class AdminUserListResponseModel(
+    val name: String,
+    val penaltyPoint: Int,
+    val role: String,
+    val userId: UUID,
+)
