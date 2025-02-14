1b4371ff46bd1bf85c6af9ce09fabcbd418eb042
kimjeakwan <s24042@gsm.hs.kr>
2025-02-14 17:18:44 +0900

    :sparkles: :: UserAPi

--- data/src/main/java/remote/api/auth/UsersAPi.kt
+++ data/src/main/java/remote/api/auth/UsersAPi.kt
@@ -2,10 +2,14 @@ package remote.api.auth
 
 import remote.dto.users.response.UsersResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.UUID
 
 interface UsersAPi {
     @GET("/{user_id}/profile")
-    suspend fun getUsers(): List<UsersResponse>
+    suspend fun getUsers(
+        @Path("user_id") userId: UUID
+    ): List<UsersResponse>
 
 
 
