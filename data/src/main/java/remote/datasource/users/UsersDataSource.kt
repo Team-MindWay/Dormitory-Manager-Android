d997e02d2a722770b6c30e03387047b2ae319938
kimjeakwan <s24042@gsm.hs.kr>
2025-02-14 17:38:16 +0900

    :recycle: :: UserId 를 추가

--- data/src/main/java/remote/datasource/users/UsersDataSource.kt
+++ data/src/main/java/remote/datasource/users/UsersDataSource.kt
@@ -2,7 +2,10 @@ package remote.datasource.users
 
 import kotlinx.coroutines.flow.Flow
 import remote.dto.users.response.UsersResponse
+import java.util.UUID
 
 interface UsersDataSource {
-    fun getUsers(): Flow<List<UsersResponse>>
+    fun getUsers(
+        userId: UUID
+    ): Flow<List<UsersResponse>>
 }
