bfaf213060ada73de2e32d22b7676fd3cbbd6279
kimjeakwan <s24042@gsm.hs.kr>
2025-02-14 17:23:48 +0900

    :memo: :: Module 에 의존성추가

--- app/src/main/java/module/RemoteDataSourceModule.kt
+++ app/src/main/java/module/RemoteDataSourceModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import remote.datasource.GAuthDataSource
+import remote.datasource.admin.AdminDataSource
+import remote.datasource.admin.AdminDataSourceImpl
 import remote.datasource.homes.HomesDataSource
 import remote.datasource.homes.HomesDataSourceImpl
 import remote.datasource.users.UsersDataSource
@@ -26,4 +28,8 @@ abstract class RemoteDataSourceModule {
     abstract fun provideHomeDatasource(
         homeDataSourceImpl: HomesDataSourceImpl
     ): HomesDataSource
+    @Binds
+    abstract fun proviedAdminDatasource(
+        adminDataSourceImpl: AdminDataSourceImpl
+    ): AdminDataSource
 }
