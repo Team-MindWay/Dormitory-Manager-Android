eeb4e89d19119503cbf9fc98f7a772f44d21e837
kimjeakwan <s24042@gsm.hs.kr>
2025-02-18 12:07:52 +0900

    :recycle: :: item, componet 분리

--- presentation/src/main/java/view/main/component/Rankingcomponent.kt
+++ presentation/src/main/java/view/main/component/Rankingcomponent.kt
@@ -9,7 +9,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,9 +24,43 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kim.presentation.R
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
 import model.rank.response.RankResponseModel
+import viewmodel.homes.uistate.HomesMyRankUiState
+import viewmodel.homes.uistate.HomesUiState
 import java.util.UUID
 
+@Composable
+internal fun RankingList(
+    modifier: Modifier = Modifier,
+    homeUiState: HomesUiState,
+    onErrorToast: (throwable: Throwable?, message: Int?) -> Unit
+) {
+    when (homeUiState) {
+        HomesUiState.Loading -> {
+            Text(text = "로딩중")
+        }
+
+        is HomesUiState.Success -> {
+            val list = homeUiState.data
+
+            RankingListItem(
+                modifier = modifier,
+                list = list.toPersistentList()
+            )
+        }
+
+        is HomesUiState.Fail -> {
+            onErrorToast(homeUiState.exception, R.string.error)
+        }
+
+        is HomesUiState.Empty -> {
+
+        }
+    }
+}
 
 @Composable
 fun RankingComponent(
@@ -50,12 +87,12 @@ fun RankingComponent(
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Text(
-                text = data.rank.toString(),
+                text = "${data.rank}위",
                 style = TextStyle(
                     fontSize = 16.sp,
                     fontWeight = FontWeight(700),
                     color = Color(0xFFFFFFFF),
-                    )
+                )
             )
             Spacer(modifier = Modifier.width(20.dp))
             Text(
@@ -64,11 +101,11 @@ fun RankingComponent(
                     fontSize = 16.sp,
                     fontWeight = FontWeight(600),
                     color = Color(0xFFFFFFFF),
-                    )
+                )
             )
             Spacer(modifier = Modifier.width(152.dp))
             Text(
-                text = data.penaltyPoint.toString(),
+                text = "${data.penaltyPoint}점",
                 style = TextStyle(
                     fontSize = 14.sp,
                     fontWeight = FontWeight(600),
@@ -76,6 +113,35 @@ fun RankingComponent(
                 )
             )
         }
+    }
+}
+
+@Composable
+internal fun RankingListItem(
+    modifier: Modifier = Modifier,
+    list: PersistentList<RankResponseModel>,
+
+    ) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(max = 10_000.dp)
+        ) {
+            items(
+                items = list,
+                key = { data ->
+                    data.userId
+                }
+            ) { data ->
+                RankingComponent(
+                    modifier = Modifier.fillMaxWidth(),
+                    data = data
+                )
+            }
+
+        }
+
     }
 }
 
