fa249a3c96faa56eb0e37ad5931a2a0e3c069f7e
jsw9436 <jswjsw101010@gmail.com>
2025-02-19 15:42:18 +0900

    ♻️ :: 쓰지않는 import문 정리

--- presentation/src/main/java/view/signin/component/DoMaInputID.kt
+++ presentation/src/main/java/view/signin/component/DoMaInputID.kt
@@ -11,8 +11,6 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
