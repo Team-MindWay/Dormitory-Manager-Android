0876deb6476e32b12c850a66a1efb8435a63d50c
jsw9436 <jswjsw101010@gmail.com>
2025-02-19 15:37:36 +0900

    💄 :: SingInSub UI 구현

---
+++ presentation/src/main/java/view/signin/screen/SignInSubScreen.kt
@@ -0,0 +1,103 @@
+package view.signin.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.msg.gauthsignin.GAuthSigninWebView
+import view.signin.component.DoMaInputID
+import view.signin.component.DoMaInputPassword
+import view.signin.component.DoMaLoginButton
+import view.theme.DoMaTypography
+
+@Composable
+fun SignInSubScreen(
+    modifier: Modifier = Modifier,
+    ClickButton: Boolean,
+    gAuthLogin: (String) -> Unit,
+    leIsClickLoginButton: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF1E1E1E))
+            .padding(horizontal = 20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "아이디",
+                    style = DoMaTypography.headlineSmall.copy(color = Color(0xFF9AD54A)),
+                )
+                Text(
+                    text = "와",
+                    style = DoMaTypography.headlineSmall.copy(color = Color(0xFFFAFAFA)),
+                )
+                Text(
+                    text = " 비밀번호",
+                    style = DoMaTypography.headlineSmall.copy(color = Color(0xFF9AD54A)),
+                )
+                Text(
+                    text = "를 입력해주세요!",
+                    style = DoMaTypography.headlineSmall.copy(color = Color(0xFFFAFAFA)),
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            DoMaInputID(
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            DoMaInputPassword(
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+
+        DoMaLoginButton(
+            onClick = leIsClickLoginButton,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 30.dp)
+        )
+
+        if (ClickButton) {
+            GAuthSigninWebView(
+                clientId = "ghskfend",
+                redirectUri = "ghskfen"
+            ) { code ->
+                gAuthLogin(code)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSignInSubScreen() {
+    SignInSubScreen(
+        ClickButton = false,
+        gAuthLogin = {},
+        leIsClickLoginButton = {}
+    )
+}
