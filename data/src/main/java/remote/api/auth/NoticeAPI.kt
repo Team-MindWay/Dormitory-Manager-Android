003e75192790a9721dfe1b1466de3122cc8fba32
kimjeakwan <s24042@gsm.hs.kr>
2025-02-20 13:42:43 +0900

    :sparkles: ::NoticeAPI.kt

---
+++ data/src/main/java/remote/api/auth/NoticeAPI.kt
@@ -0,0 +1,14 @@
+package remote.api.auth
+
+import remote.dto.users.response.NoticeResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.util.UUID
+
+interface NoticeAPI {
+    @GET("/{notice_id}")
+    suspend fun getNotice(
+        @Path("notice_id") noticeId: UUID,
+    ): List<NoticeResponse>
+
+}
