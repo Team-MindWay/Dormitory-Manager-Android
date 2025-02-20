4432d9f74e0e47fd6c78f4add66e0c8497b3d76d
jsw9436 <jswjsw101010@gmail.com>
2025-02-20 16:54:42 +0900

    ♻️ :: 불필요한 import문 정리

--- presentation/src/main/java/view/signin/screen/SignInScreen.kt
+++ presentation/src/main/java/view/signin/screen/SignInScreen.kt
@@ -6,9 +6,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
