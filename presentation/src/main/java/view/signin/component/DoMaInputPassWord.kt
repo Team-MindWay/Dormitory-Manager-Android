8df7e0cd90852731ed065cff5ac9d13e4085416b
jsw9436 <jswjsw101010@gmail.com>
2025-02-24 23:50:38 +0900

    💄 :: DoMaInputPassWord 구현

---
+++ presentation/src/main/java/view/signin/component/DoMaInputPassWord.kt
@@ -0,0 +1,69 @@
+
+package view.signin.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import view.theme.DoMaAndroidTheme
+import view.theme.DoMaTypography
+
+@Composable
+fun DoMaInputPassword(
+    modifier: Modifier = Modifier,
+) {
+    val idState = remember { mutableStateOf(TextFieldValue("")) } // viewmodel 구현 후 수정 하겠습니다.
+
+    DoMaAndroidTheme { colors, typography ->
+
+        Column(
+            modifier = Modifier
+        ) {
+            Text(
+                text = "비밀번호",
+                style = DoMaTypography.bodyMedium.copy(color = colors.WHITE)
+            )
+            Spacer(
+                modifier = Modifier.height(4.dp)
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = idState.value,
+                onValueChange = { idState.value = it },
+                textStyle = DoMaTypography.labelLarge.copy(color = colors.WHITE),
+                shape = RoundedCornerShape(12.dp),
+                singleLine = true,
+                placeholder = {
+                    Text(
+                        text = "비밀번호를 입력해주세요.",
+                        style = DoMaTypography.labelLarge.copy(color = colors.silver)
+                    )
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    backgroundColor = Color(0xFF1E1E1E),
+                    cursorColor = Color.White,
+                    focusedBorderColor = Color(0xFFB9B9B9),
+                    unfocusedBorderColor = Color(0xFFB9B9B9)
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDoMaInputPassword() {
+    DoMaInputPassword()
+}
