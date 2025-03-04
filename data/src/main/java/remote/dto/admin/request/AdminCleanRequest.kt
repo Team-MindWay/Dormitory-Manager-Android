4158eab7c61bc78ad4a8de4dc08710717ec62f0e
kimjeakwan <s24042@gsm.hs.kr>
2025-03-04 18:08:48 +0900

    :recycle: :: AdminCleanRequest authority,accountIdx

--- data/src/main/java/remote/dto/admin/request/AdminCleanRequest.kt
+++ data/src/main/java/remote/dto/admin/request/AdminCleanRequest.kt
@@ -2,21 +2,29 @@ package remote.dto.admin.request
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import emumtype.Authority
 import model.admin.request.AdminCleanRequestModel
+import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class AdminCleanRequest(
     @Json(name = "toDayClean") val toDayClean: String,
     @Json(name = "cleanPoint") val cleanPoint: Int,
+    @Json(name = "accountIdx") val accountIdx: UUID,
+    @Json(name = "authority") val authority: Authority,
 )
 
 fun AdminCleanRequestModel.toDto() = AdminCleanRequest(
     toDayClean = toDayClean,
     cleanPoint = cleanPoint,
+    accountIdx = accountIdx,
+    authority = authority
 )
 
 fun AdminCleanRequest.toModel() = AdminCleanRequestModel(
     toDayClean = toDayClean,
     cleanPoint = cleanPoint,
+    accountIdx = accountIdx,
+    authority = authority
 )
 
