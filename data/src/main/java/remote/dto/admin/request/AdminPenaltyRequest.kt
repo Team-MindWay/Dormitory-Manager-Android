ebaf4504456b927098850f6303e4ee691f9fa62b
kimjeakwan <s24042@gsm.hs.kr>
2025-03-04 18:11:30 +0900

    :recycle: :: AdminPenaltyRequest

--- data/src/main/java/remote/dto/admin/request/AdminPenaltyRequest.kt
+++ data/src/main/java/remote/dto/admin/request/AdminPenaltyRequest.kt
@@ -2,21 +2,30 @@ package remote.dto.admin.request
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import emumtype.Authority
 import model.admin.request.AdminPenaltyRequestModel
-import model.admin.response.AdminPenaltyResponseModel
+import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class AdminPenaltyRequest (
     @Json(name = "penaltyPoint") val penaltyPoint: Int,
     @Json(name = "because") val because: String,
+    @Json(name = "accountIdx") val accountIdx: UUID,
+    @Json(name = "authority") val authority: Authority
+
 )
 
 fun AdminPenaltyRequestModel.toDto() = AdminPenaltyRequest(
     because = because,
     penaltyPoint = penaltyPoint,
+    accountIdx = accountIdx,
+    authority = authority
 )
 
 fun AdminPenaltyRequest.toModel() = AdminPenaltyRequestModel(
     because = because,
     penaltyPoint = penaltyPoint,
+    accountIdx = accountIdx,
+    authority = authority
+
 )
