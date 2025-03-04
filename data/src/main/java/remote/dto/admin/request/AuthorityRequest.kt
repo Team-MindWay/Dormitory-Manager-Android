c6f0523cd812760d8d3e605688556bdf5c2d5734
kimjeakwan <s24042@gsm.hs.kr>
2025-03-04 18:16:26 +0900

    :recycle: :: AuthorityRequest

---
+++ data/src/main/java/remote/dto/admin/request/AuthorityRequest.kt
@@ -0,0 +1,23 @@
+package remote.dto.admin.request
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import model.admin.request.AdminPenaltyRequestModel
+import model.admin.request.AuthorityRequestModel
+
+@JsonClass(generateAdapter = true)
+data class AuthorityRequest(
+    @Json(name = "accountIdx") val accountIdx: String,
+    @Json(name = "authority") val authority: String
+)
+
+fun AuthorityRequestModel.toDto() = AuthorityRequest(
+    accountIdx = accountIdx,
+    authority = authority
+)
+
+fun AuthorityRequest.toModel() = AuthorityRequestModel(
+    accountIdx = accountIdx,
+    authority = authority
+
+)
