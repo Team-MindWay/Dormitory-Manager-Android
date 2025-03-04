cc07cf46e8b3c71557611621de5324c5b8fa57a7
kimjeakwan <s24042@gsm.hs.kr>
2025-03-04 18:11:58 +0900

    :recycle: :: AdminPenaltyRequestModel

--- domain/src/main/java/model/admin/request/AdminPenaltyRequestModel.kt
+++ domain/src/main/java/model/admin/request/AdminPenaltyRequestModel.kt
@@ -1,6 +1,11 @@
 package model.admin.request
 
+import emumtype.Authority
+import java.util.UUID
+
 data class AdminPenaltyRequestModel (
      val penaltyPoint: Int,
      val because: String,
+     val accountIdx: UUID,
+     val authority: Authority
 )
