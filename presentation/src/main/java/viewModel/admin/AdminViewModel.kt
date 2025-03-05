0ff5587ff9fd546a19e5164e234c4b277295a7f0
kimjeakwan <s24042@gsm.hs.kr>
2025-03-05 20:09:32 +0900

    :recycle: :: AdminViewModel 공백 삭제

--- presentation/src/main/java/viewModel/admin/AdminViewModel.kt
+++ presentation/src/main/java/viewModel/admin/AdminViewModel.kt
@@ -1,6 +1,6 @@
 package viewmodel.admin
 
-import Untill.asResult
+import until.asResult
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -26,7 +26,7 @@ class AdminViewModel @Inject constructor(
     private val postPenaltyUseCase: PostPenaltyUseCase,
     private val getPenaltyListUseCase: GetPenaltyListUseCase,
     private val postCleanUseCase: PostCleanUseCase
-    ) : ViewModel() {
+) : ViewModel() {
 
     private val _adminUiState = MutableStateFlow<AdminUiState>(AdminUiState.Empty)
     val adminUiState: StateFlow<AdminUiState> = _adminUiState.asStateFlow()
@@ -49,16 +49,61 @@ class AdminViewModel @Inject constructor(
             .asResult()
             .collectLatest { result ->
                 when (result) {
-                    is Untill.Result.Success -> {
+                    is until.Result.Success -> {
                         _getUserUiState.value = GetUserUiState.Success(result.data)
                     }
 
-                    is Untill.Result.Loading -> {
+                    is until.Result.Loading -> {
                         _getUserUiState.value = GetUserUiState.Loading
                     }
 
-                    is Untill.Result.Error -> {
+                    is until.Result.Error -> {
                         _getUserUiState.value = GetUserUiState.Fail
+                    }
+                }
+            }
+    }
+
+    internal fun postPenalty(userId: String, body: AdminPenaltyRequestModel) =
+        viewModelScope.launch {
+            postPenaltyUseCase(
+                userId = userId,
+                body = body
+            )
+                .asResult()
+                .collectLatest { result ->
+                    when (result) {
+                        is until.Result.Success -> {
+                            _adminUiState.value = AdminUiState.Success(result.data)
+                        }
+
+                        is until.Result.Loading -> {
+                            _adminUiState.value = AdminUiState.Loading
+                        }
+
+                        is until.Result.Error -> {
+                            _adminUiState.value = AdminUiState.Fail
+                        }
+
+                    }
+                }
+        }
+
+    internal fun getPenaltyList() = viewModelScope.launch {
+        getPenaltyListUseCase()
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is until.Result.Success -> {
+                        _getPenaltyListUiState.value = GetPenaltyListUiState.Success(result.data)
+                    }
+
+                    is until.Result.Loading -> {
+                        _getPenaltyListUiState.value = GetPenaltyListUiState.Loading
+                    }
+
+                    is until.Result.Error -> {
+                        _getPenaltyListUiState.value = GetPenaltyListUiState.Fail
                     }
 
                 }
@@ -66,77 +111,26 @@ class AdminViewModel @Inject constructor(
             }
     }
 
-    internal fun postPenalty(userId: String, body: AdminPenaltyRequestModel) = viewModelScope.launch {
-        postPenaltyUseCase(
+    internal fun postClean(userId: String, body: AdminCleanRequestModel) = viewModelScope.launch {
+        postCleanUseCase(
             userId = userId,
             body = body
         )
             .asResult()
             .collectLatest { result ->
                 when (result) {
-                    is Untill.Result.Success -> {
-                        _adminUiState.value = AdminUiState.Success(result.data)
-                    }
-
-                    is Untill.Result.Loading -> {
-                        _adminUiState.value =  AdminUiState.Loading
-                    }
-
-                    is Untill.Result.Error -> {
-                        _adminUiState.value = AdminUiState.Fail
-                    }
-
-                }
-
-            }
-    }
-    internal fun getPenaltyList() = viewModelScope.launch {
-        getPenaltyListUseCase()
-            .asResult()
-            .collectLatest { result ->
-                when (result) {
-                    is Untill.Result.Success -> {
-                        _getPenaltyListUiState.value = GetPenaltyListUiState.Success(result.data)
-                    }
-
-                    is Untill.Result.Loading -> {
-                        _getPenaltyListUiState.value =   GetPenaltyListUiState.Loading
-                    }
-
-                    is Untill.Result.Error -> {
-                        _getPenaltyListUiState.value =  GetPenaltyListUiState.Fail
-                    }
-
-                }
-
-            }
-    }
-    internal fun postClean(userId: String, body: AdminCleanRequestModel) = viewModelScope.launch {
-       postCleanUseCase(
-           userId = userId,
-           body = body
-       )
-            .asResult()
-            .collectLatest { result ->
-                when (result) {
-                    is Untill.Result.Success -> {
+                    is until.Result.Success -> {
                         _postCleanUiState.value = PostCleanUiState.Success(result.data)
                     }
 
-                    is Untill.Result.Loading -> {
-                        _postCleanUiState.value =   PostCleanUiState.Loading
+                    is until.Result.Loading -> {
+                        _postCleanUiState.value = PostCleanUiState.Loading
                     }
 
-                    is Untill.Result.Error -> {
+                    is until.Result.Error -> {
                         _postCleanUiState.value = PostCleanUiState.Fail
                     }
-
                 }
-
             }
     }
-
-
-
-
 }
