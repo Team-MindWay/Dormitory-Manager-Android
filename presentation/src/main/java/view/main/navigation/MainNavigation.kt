158d4731bbd7a70a42e4461fc874246831275ec8
kimjeakwan <s24042@gsm.hs.kr>
2025-03-05 20:51:02 +0900

    :recycle: :: MainNavigation PostDemeritSearchRounte

--- presentation/src/main/java/view/main/navigation/MainNavigation.kt
+++ presentation/src/main/java/view/main/navigation/MainNavigation.kt
@@ -2,24 +2,32 @@ package view.main.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import view.main.Screen.MainRoute
-import view.main.Screen.MainScreen
+import view.main.screen.PostDemeritSearchRoute
+
 
 const val mainRoute = "main_route"
+const val demeritPostRoute = "demerit_post_route"
+const val studentPatchRoute = "student_patch_route"
 
-fun  NavController.navigationToMain(){
+
+fun NavController.navigationToMain() {
     this.navigate(mainRoute)
 }
-fun NavGraphBuilder.mainScreen(
+
+fun NavController.demeritPostRoute(navOptions: NavOptions? = null) {
+    this.navigate(demeritPostRoute, navOptions)
+}
+
+fun NavGraphBuilder.postDemeritScreen(
     navigateToBack: () -> Unit,
     onErrorToast: (throwable: Throwable?, message: Int?) -> Unit
-
-){
-    composable(mainRoute){
-        MainRoute(
+) {
+    composable(demeritPostRoute) {
+        PostDemeritSearchRoute(
             navigateToBack = navigateToBack,
-            onErrorToast = onErrorToast
+            onErrorToast = onErrorToast,
         )
     }
 }
