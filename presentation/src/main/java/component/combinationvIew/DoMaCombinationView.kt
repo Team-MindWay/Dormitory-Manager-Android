57826a1b7c6e47978fb5035f79c100f596b011a1
kimjeakwan <s24042@gsm.hs.kr>
2025-03-05 20:12:07 +0900

    :recycle: :: DoMaCombinationView 경로 변경

--- presentation/src/main/java/component/combinationvIew/DoMaCombinationView.kt
+++ presentation/src/main/java/component/combinationvIew/DoMaCombinationView.kt
@@ -1,30 +1,19 @@
-package component.combinationVIew
+package component.combinationvIew
 
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
-import component.modifier.DomaNavBar
+import component.navigationbar.DomaNavBar
 import emumtype.DomaNavBarItemType
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import view.Mypage.Screen.MyPageRoute
-import view.main.Screen.MainRoute
+import view.mypage.Screen.MyPageRoute
+import view.main.screen.MainRoute
 import view.notice.screen.NoticeRoute
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DomaCombinationView(
     modifier: Modifier = Modifier,
-    coroutine : CoroutineScope = rememberCoroutineScope(),
     currentDestination: DomaNavBarItemType,
     setCurrentDestination: (DomaNavBarItemType) -> Unit,
     navigateToNotice: () -> Unit,
@@ -63,7 +52,6 @@ fun DomaCombinationView(
                     onErrorToast = onErrorToast,
                     navigateToBack = navigateToBack
                 )
-
             }
         }
     }
