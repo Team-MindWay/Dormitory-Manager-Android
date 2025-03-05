cd10d70ce7efc8f745468a524e321f8e26dfba1a
kimjeakwan <s24042@gsm.hs.kr>
2025-03-05 20:13:28 +0900

    :recycle: ::  authority 권한 추가

--- data/src/main/java/remote/dto/auth/response/GAuthLoginResponse.kt
+++ data/src/main/java/remote/dto/auth/response/GAuthLoginResponse.kt
@@ -3,6 +3,7 @@ package remote.dto.auth.response
 import model.auth.response.GAuthLoginResponseModel
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import emumtype.Authority
 
 @JsonClass(generateAdapter = true)
 data class GAuthLoginResponse (
@@ -10,11 +11,14 @@ data class GAuthLoginResponse (
     @Json(name = "refreshToken") val refreshToken: String,
     @Json(name = "accessTokenExpiresIn") val accessTokenExpiresIn: String,
     @Json(name = "refreshTokenExpiresIn") val refreshTokenExpiresIn: String,
+    @Json(name = "Authority") val authority: Authority
+
 )
 fun GAuthLoginResponse.toLogin()= GAuthLoginResponseModel(
     accessToken=accessToken,
     refreshToken=refreshToken,
     accessTokenExpiresIn=accessTokenExpiresIn,
-    refreshTokenExpiresIn=refreshTokenExpiresIn
+    refreshTokenExpiresIn=refreshTokenExpiresIn,
+    authority=authority
 )
 
