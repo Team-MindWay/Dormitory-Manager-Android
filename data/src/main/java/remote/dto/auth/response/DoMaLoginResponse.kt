3f5bb827be909ba5b01635634b120a1d3cc72d11
kimjeakwan <s24042@gsm.hs.kr>
2025-03-07 15:36:05 +0900

    :recycle: :: 공백삭제

--- data/src/main/java/remote/dto/auth/response/DoMaLoginResponse.kt
+++ data/src/main/java/remote/dto/auth/response/DoMaLoginResponse.kt
@@ -1,20 +1,20 @@
 package remote.dto.auth.response
 
-import model.auth.response.GAuthLoginResponseModel
+import model.auth.response.DoMaLoginResponseModel
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import emumtype.Authority
 
 @JsonClass(generateAdapter = true)
-data class GAuthLoginResponse (
+data class DoMaLoginResponse(
     @Json(name = "accessToken") val accessToken: String,
     @Json(name = "refreshToken") val refreshToken: String,
     @Json(name = "accessTokenExpiresIn") val accessTokenExpiresIn: String,
     @Json(name = "refreshTokenExpiresIn") val refreshTokenExpiresIn: String,
     @Json(name = "Authority") val authority: Authority
-
 )
-fun GAuthLoginResponse.toLogin()= GAuthLoginResponseModel(
+
+fun DoMaLoginResponseModel.toLogin()= DoMaLoginResponseModel(
     accessToken=accessToken,
     refreshToken=refreshToken,
     accessTokenExpiresIn=accessTokenExpiresIn,
