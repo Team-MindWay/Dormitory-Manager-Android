06b6356244fc1f21ba9469e5eb89d9d743719385
kimjeakwan <s24042@gsm.hs.kr>
2025-03-07 16:04:35 +0900

    :recycle: :: 공백삭제

--- presentation/src/main/java/view/main/navigation/MainNavigation.kt
+++ presentation/src/main/java/view/main/navigation/MainNavigation.kt
@@ -6,11 +6,9 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import view.main.screen.PostDemeritSearchRoute
 
-
 const val mainRoute = "main_route"
 const val demeritPostRoute = "demerit_post_route"
 const val studentPatchRoute = "student_patch_route"
-
 
 fun NavController.navigationToMain() {
     this.navigate(mainRoute)
