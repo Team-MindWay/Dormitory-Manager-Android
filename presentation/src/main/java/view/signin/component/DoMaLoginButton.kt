e5ecf96abe0637ef410c8c8f664252a7f754394c
jsw9436 <jswjsw101010@gmail.com>
2025-03-07 15:37:40 +0900

    :recycle: :: 쓰지 않는 import문 정리

--- presentation/src/main/java/view/signin/component/DoMaLoginButton.kt
+++ presentation/src/main/java/view/signin/component/DoMaLoginButton.kt
@@ -8,8 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
