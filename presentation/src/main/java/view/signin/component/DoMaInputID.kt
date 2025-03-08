80a8ade448d8ace36ca92d6f29a43f78abd55846
jsw9436 <jswjsw101010@gmail.com>
2025-03-08 14:11:23 +0900

    :recycle: :: 사용하지 않는 import문 삭제

--- presentation/src/main/java/view/signin/component/DoMaInputID.kt
+++ presentation/src/main/java/view/signin/component/DoMaInputID.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
